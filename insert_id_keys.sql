6a31c25d41215823062047cf5823a8676f75d45d
lkindo <lkindo@gmail.com>
2026-02-07 16:16:12 +0900

    로그파일정리

--- insert_id_keys.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO ecopseq (table_name, next_id) VALUES ('REPRT_STATS_ID', 1) ON CONFLICT (table_name) DO NOTHING;
-INSERT INTO ecopseq (table_name, next_id) VALUES ('DTA_USE_STATS_ID', 1) ON CONFLICT (table_name) DO NOTHING;
