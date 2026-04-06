da58249d84f3b21b093d2f62c52fee0fee7a4b14
lkindo <lkindo@gmail.com>
2026-04-07 07:39:26 +0900

    1

--- tmp/fix_bom.ps1
+++
@@ -1,6 +0,0 @@
-$files = Get-ChildItem -Path d:\project\egov-enterprise\foundation\src\test\java -Filter *.java -Recurse
-foreach ($file in $files) {
-    Write-Host "Fixing BOM in $($file.FullName)"
-    $content = Get-Content -Raw -Encoding UTF8 $file.FullName
-    [System.IO.File]::WriteAllText($file.FullName, $content, (New-Object System.Text.UTF8Encoding($false)))
-}
