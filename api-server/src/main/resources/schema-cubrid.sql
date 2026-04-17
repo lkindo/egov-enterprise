04f2a50bc2f4981da1563a30e1556df25ea32d29
lkindo <lkindo@gmail.com>
2026-04-17 16:58:39 +0900

    불필요 가비지 egov파일들 삭제

--- api-server/src/main/resources/schema-cubrid.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS ids (
-    table_name VARCHAR(16) PRIMARY KEY,
-    next_id NUMERIC(30) NOT NULL
-);
-
-INSERT INTO ids (table_name, next_id) 
-SELECT 'SAMPLE', 1 
-WHERE NOT EXISTS (SELECT 1 FROM ids WHERE table_name = 'SAMPLE');
